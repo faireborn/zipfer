567aa7ef336a7ed0f27f1b1a7f329c7a3e71e045
Ryuto Saito <r.saito0111@gmail.com>
2025-11-17 20:32:08 +0900

    refactor mean function

--- src/util.zig
+++ src/util.zig
@@ -1,22 +1,15 @@
 const std = @import("std");
 
 pub fn mean(comptime T: type, comptime ResultT: type, xs: []const T) ResultT {
-    var sum: T = 0;
+    var sum: ResultT = 0;
     for (xs) |x| {
-        sum += x;
+        switch (@typeInfo(T)) {
+            .int => sum += @floatFromInt(x),
+            .float => sum += @floatCast(x),
+            else => @compileError("Type Error"),
+        }
     }
-
-    switch (@typeInfo(T)) {
-        .int => {
-            return @as(ResultT, @floatFromInt(sum)) / @as(ResultT, @floatFromInt(xs.len));
-        },
-        .float => {
-            return sum / @as(ResultT, @floatFromInt(xs.len));
-        },
-        else => {
-            @compileError("Type Error");
-        },
-    }
+    return sum / @as(ResultT, @floatFromInt(xs.len));
 }
 
 test "mean" {
@@ -45,18 +38,18 @@ test "mean" {
         try std.testing.expectEqual(expect, res);
     }
     {
-        const xs = &[_]f64{ 0, 0, 0, 0 };
-        const expect: f64 = 0;
+        const xs = &[_]f32{ 43.43, 213, 51, -328.18, -318.329 };
+        const expect: f32 = -67.8158;
 
-        const res = mean(f64, f64, xs);
+        const res = mean(f32, f32, xs);
 
         try std.testing.expectEqual(expect, res);
     }
     {
-        const xs = &[_]f32{ 0, 0, 0, 0 };
-        const expect: f64 = 0;
+        const xs = &[_]f64{ 21.00, -213.25, -2199.01, 1203.2, 0.32 };
+        const expect: f64 = -237.54800000000006;
 
-        const res = mean(f32, f64, xs);
+        const res = mean(f64, f64, xs);
 
         try std.testing.expectEqual(expect, res);
     }
