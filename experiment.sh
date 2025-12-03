f0676c905a9036144296374feb8fb915fac26c13
faireborn <r.saito0111@gmail.com>
2025-12-04 00:46:53 +0900

    run in parallel with xargs

--- experiment.sh
+++ experiment.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -euo pipefail
+
 ALGORITHM_LIST=(
   "unigram"
   "bpe"
@@ -32,6 +34,7 @@ mkdir -p \
 
 printf "Training tokenizers...\n"
 
+TRAINING_JOBS=()
 for ALGORITHM in "${ALGORITHM_LIST[@]}"; do
   for VOCAB_SIZE in "${VOCAB_SIZE_LIST[@]}"; do
 
@@ -40,42 +43,43 @@ for ALGORITHM in "${ALGORITHM_LIST[@]}"; do
       [ -f "${TRAINED_DIR}"/"${ALGORITHM}"_"${VOCAB_SIZE}".vocab ] &&
       continue
 
-    ./tokenizer_train.sh -a ${ALGORITHM} -s ${VOCAB_SIZE} -i ${INPUT} -t ${TRAINED_DIR} ||
-      {
-        echo "Error while training tokenizers!"
-        exit 1
-      }
+    TRAINING_JOBS+=("./tokenizer_train.sh -a ${ALGORITHM} -s ${VOCAB_SIZE} -i ${INPUT} -t ${TRAINED_DIR}")
   done
 done
 
+# start training jobs
+printf "%s\n" "${TRAINING_JOBS[@]}" | xargs -P "$(nproc)" -I{} bash -c "{}"
+
 printf "\n\nTokenizing the corpus...\n"
+
+TOKENIZING_JOBS=()
 for ALGORITHM in "${ALGORITHM_LIST[@]}"; do
   for VOCAB_SIZE in "${VOCAB_SIZE_LIST[@]}"; do
 
     # Skip the encoding step since the tokenizer has already tokenized the corpus
     [ -f "${ENCODED_DIR}"/"${ALGORITHM}"_"${VOCAB_SIZE}".txt ] && continue
 
-    ./tokenizer_encode.sh -a ${ALGORITHM} -s ${VOCAB_SIZE} -i ${INPUT} -t ${TRAINED_DIR} -e ${ENCODED_DIR} ||
-      {
-        echo "Error while tokenizing the corpus!"
-        exit 1
-      }
+    TOKENIZING_JOBS+=("./tokenizer_encode.sh -a ${ALGORITHM} -s ${VOCAB_SIZE} -i ${INPUT} -t ${TRAINED_DIR} -e ${ENCODED_DIR}")
   done
 done
 
+# start tokenizing jobs
+printf "%s\n" "${TOKENIZING_JOBS[@]}" | xargs -P "$(nproc)" -I{} bash -c "{}"
+
 printf "\n\nEvaluating tokenizers...\n"
+
+EVALUATING_JOBS=()
 for ALGORITHM in "${ALGORITHM_LIST[@]}"; do
   for VOCAB_SIZE in "${VOCAB_SIZE_LIST[@]}"; do
 
     # Skip the evaluating step since the tokenizer is already evaluated
     [ -d "${RESULTS_DIR}"/"${ALGORITHM}"_"${VOCAB_SIZE}" ] && continue
 
-    ./zipfer.sh -a ${ALGORITHM} -s ${VOCAB_SIZE} -e ${ENCODED_DIR} -r ${RESULTS_DIR} ||
-      {
-        echo "Error while evaluating tokenizers!"
-        exit 1
-      }
+    EVALUATING_JOBS+=("./zipfer.sh -a ${ALGORITHM} -s ${VOCAB_SIZE} -e ${ENCODED_DIR} -r ${RESULTS_DIR}")
   done
 done
+
+# start evaluating jobs
+printf "%s\n" "${EVALUATING_JOBS[@]}" | xargs -P "$(nproc)" -I{} bash -c "{}"
 
 printf "\n\nDone!\n"
